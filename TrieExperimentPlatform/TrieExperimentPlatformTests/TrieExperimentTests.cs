9924d159a45bd3b714696494132a4a30b8398837
Andrew Gawronski <andrewgawronski@gmail.com>
2015-05-20 22:31:10 -0700

    Initial project and test project commit.

---
+++ TrieExperimentPlatform/TrieExperimentPlatformTests/TrieExperimentTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrieExperimentPlatformTests
+{
+    [TestClass]
+    public class TrieExperimentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
