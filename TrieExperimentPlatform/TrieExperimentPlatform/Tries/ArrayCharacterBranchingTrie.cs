c098ec1a0446cc63ad971b6474828348caf8e2b3
Andrew Gawronski <andrewgawronski@gmail.com>
2015-05-20 23:10:45 -0700

    Add placeholders for new tries

---
+++ TrieExperimentPlatform/TrieExperimentPlatform/Tries/ArrayCharacterBranchingTrie.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrieExperimentPlatform.Tries
+{
+    // Andrew Implement:
+    class ArrayCharacterBranchingTrie : ITrie
+    {
+        public void InsertWithPriority(string value, double priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FindWithPrefix(string prefix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> FindTopNWithPrefix(string prefix, int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ArrayCharacterBranchingTrie()
+        {
+
+        }
+
+
+        class ArrayNode
+        {
+            public string Value { get; set; }
+
+            public double Priority { get; set; }
+
+            public ArrayNode[] Children { get; set; }
+
+            public ArrayNode(string value, double priority)
+            {
+                Value = value;
+                Priority = priority;
+                Children = new ArrayNode[27]; // # lowercase chars + space
+            }
+        }
+
+
+
+    }
+}
