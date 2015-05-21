e2b61937e37cedae590224f7596de9bd97907e1c
Andrew Gawronski <andrewgawronski@gmail.com>
2015-05-20 22:45:46 -0700

    Added simple interface for tries

---
+++ TrieExperimentPlatform/TrieExperimentPlatform/Tries/ITrie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrieExperimentPlatform.Tries
+{
+    interface ITrie
+    {
+        void InsertWithPriority(string value, double priority);
+
+        string FindWithPrefix(string prefix);
+
+        IList<string> FindTopNWithPrefix(string prefix, int n);
+    }
+}
