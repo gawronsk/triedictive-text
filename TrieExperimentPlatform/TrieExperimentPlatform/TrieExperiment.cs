5dc5cf29ad8c407192320749bccd838db5f86e2b
Andrew Gawronski <andrewgawronski@gmail.com>
2015-05-23 23:23:39 -0700

    Adding basic test code.

--- TrieExperimentPlatform/TrieExperimentPlatform/TrieExperiment.cs
+++ TrieExperimentPlatform/TrieExperimentPlatform/TrieExperiment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace TrieExperimentPlatform
     {
         static void Main(string[] args)
         {
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\Data\count_1w_small.txt");
+            Console.WriteLine("Read {0} lines from file", lines.Length);
+
+            Stopwatch s = Stopwatch.StartNew();
+
+            // build trie
+            foreach(var line in lines)
+            {
+                // add to trie
+            }
+
+            s.Stop();
+            Console.WriteLine("Elapsed Time Building Tree: {0} ms", s.ElapsedMilliseconds);
+
+            s = Stopwatch.StartNew();
+
+            //run experiment
+
+            s.Stop();
+            Console.WriteLine("Elapsed Time Running Experiment: {0} ms", s.ElapsedMilliseconds);
+
+            Console.Read();
         }
     }
 }
